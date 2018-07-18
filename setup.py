2e14abac51ffe5d313e0c5cb42eba3a18b558e6c
JoMingyu <city7310@naver.com>
2018-07-18 23:07:05 +0900

    [ADD] setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='flask-validator-extended',
+    description='A Pythonic way for validate requested JSON payload of Flask',
+    version='0.0.1',
+    url='https://github.com/JoMingyu/Flask-Validator',
+    license='Apache License 2.0',
+    author='PlanB',
+    author_email='mingyu.planb@gmail.com',
+    maintainer='PlanB',
+    maintainer_email='mingyu.planb@gmail.com',
+    install_requires=[
+        'Flask'
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    packages=['flask_validator']
+)
