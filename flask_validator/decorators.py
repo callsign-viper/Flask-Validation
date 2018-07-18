f76d5662c8c3852d7911ab6e410d97df157e3488
JoMingyu <city7310@naver.com>
2018-07-19 08:44:10 +0900

    [DONE] validate_common

--- flask_validator/decorators.py
+++ flask_validator/decorators.py
@@ -1,1 +1,32 @@
+from functools import wraps
+
+from flask import abort, request
+
+
+def validate_common(
+        key_type_mapping: dict, # {'': type, '': type, ...}
+        code_when_content_type_is_not_json=406,
+        key_missing_code=400,
+        invalid_type_code=400):
+
+    def validate_key_and_type(src, mapping):
+        for key, typ in mapping.items():
+            if key not in src:
+                # 전형적인 bad request
+                abort(key_missing_code)
+            elif type(src[key]) is not typ:
+                abort(invalid_type_code)
+
+    def decorator(fn):
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            if not request.is_json:
+                abort(code_when_content_type_is_not_json)
+            else:
+                if key_type_mapping:
+                    validate_key_and_type(request.json, key_type_mapping)
+
+            return fn(*args, **kwargs)
+        return wrapper
+    return decorator
 
