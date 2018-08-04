d23344e4698a26c89c07791bd8312d1e1b789633
JoMingyu <city7310@naver.com>
2018-08-04 16:14:24 +0900

    [DONE] BaseTestCase 추가
    - 특정 데코레이터가 붙어 있는 view function(POST /)이 register된 Flask 앱의 test client를 반환하는 헬퍼 함수 포함

---
+++ test.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+
+from flask import Flask
+from flask.testing import FlaskClient
+
+from flask_validator import *
+
+
+class BaseTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def _get_test_client_of_decorated_view_function_registered_flask_app(self, decorator_func) -> FlaskClient:
+        def view_func():
+            return 'hello'
+
+        decorated_view_func = decorator_func(view_func)
+        app = Flask(__name__)
+
+        app.add_url_rule('/', view_func=decorated_view_func, methods=['POST'])
+
+        return app.test_client()
+
